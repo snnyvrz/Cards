40bbc828953fbb98e9bf43ce75a26e887fefbfe5
sinaniya <sinaniya@gmail.com>
2021-01-23 11:40:30 +0330

    added fields and constructors

--- bin/Cards.dart
+++ bin/Cards.dart
@@ -1,7 +1,23 @@
 class Deck {
+  List<Card> cards = [];
 
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five',
+    'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+    var suits = ['Diamonds', 'Spades', 'Clubs', 'Hearts'];
+
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
 }
 
 class Card {
+  String rank;
+  String suit;
 
+  Card(this.rank, this.suit);
 }
